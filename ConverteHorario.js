b66ba8513c6c595b5edbf7af946d4ada190ddd3a
KloatzUwU <kawan.borges.unico@gmail.com>
2023-05-11 14:43:00 -0300

    corrigindo os ultimos bugs do projeto

--- ConverteHorario.js
+++ ConverteHorario.js
@@ -140,16 +140,74 @@ function ConverteHorario(time, timezone){
     }
 
     if(timezone === "pst"){
-        let horarioEEST = time - 2;
-        let horarioBRT = time + 4;
-        const horariosConvertidos = ([{
-            time: horarioEEST,
-            timezone: "eest", 
-        },{
-            time: horarioBRT,
-            timezone: "brt",
-        }])
-        return horariosConvertidos;
+        if(time >= 24 || time < 0){
+            return `o horário passado não é valido, por favor utilize o sistema 24 horas`
+        }else{
+            if(time >= 0 && time <= 23){
+                let horaVariavelEEST;
+                let horaVariavelBRT;
+                time == 0 ? horaVariavelEEST = 14 : horaVariavelEEST;
+                time == 1 ? horaVariavelEEST = 13 : horaVariavelEEST;
+                time == 2 ? horaVariavelEEST = 12 : horaVariavelEEST;
+                time == 3 ? horaVariavelEEST = 11 : horaVariavelEEST;
+                time == 4 ? horaVariavelEEST = 10 : horaVariavelEEST;
+                time == 5 ? horaVariavelEEST = 9 : horaVariavelEEST;
+                time == 6 ? horaVariavelEEST = 8 : horaVariavelEEST;
+                time == 7 ? horaVariavelEEST = 7 : horaVariavelEEST;
+                time == 8 ? horaVariavelEEST = 6 : horaVariavelEEST;
+                time == 9 ? horaVariavelEEST = 5 : horaVariavelEEST;
+                time == 10 ? horaVariavelEEST = 4 : horaVariavelEEST;
+                time == 11 ? horaVariavelEEST = 3 : horaVariavelEEST;
+                time == 12 ? horaVariavelEEST = 2 : horaVariavelEEST;
+                time == 13 ? horaVariavelEEST = 1 : horaVariavelEEST;
+                time == 14 ? horaVariavelEEST = 24 : horaVariavelEEST;
+                time == 15 ? horaVariavelEEST = 23 : horaVariavelEEST;
+                time == 16 ? horaVariavelEEST = 22 : horaVariavelEEST;
+                time == 17 ? horaVariavelEEST = 21 : horaVariavelEEST;
+                time == 18 ? horaVariavelEEST = 20 : horaVariavelEEST;
+                time == 19 ? horaVariavelEEST = 19 : horaVariavelEEST;
+                time == 20 ? horaVariavelEEST = 18 : horaVariavelEEST;
+                time == 21 ? horaVariavelEEST = 17 : horaVariavelEEST;
+                time == 22 ? horaVariavelEEST = 16 : horaVariavelEEST;
+                time == 23 ? horaVariavelEEST = 15 : horaVariavelEEST;
+
+                time == 0 ? horaVariavelBRT = 20 : horaVariavelBRT;
+                time == 1 ? horaVariavelBRT = 19 : horaVariavelBRT;
+                time == 2 ? horaVariavelBRT = 18 : horaVariavelBRT;
+                time == 3 ? horaVariavelBRT = 17 : horaVariavelBRT;
+                time == 4 ? horaVariavelBRT = 16 : horaVariavelBRT;
+                time == 5 ? horaVariavelBRT = 15 : horaVariavelBRT;
+                time == 6 ? horaVariavelBRT = 14 : horaVariavelBRT;
+                time == 7 ? horaVariavelBRT = 13 : horaVariavelBRT;
+                time == 8 ? horaVariavelBRT = 12 : horaVariavelBRT;
+                time == 9 ? horaVariavelBRT = 11 : horaVariavelBRT;
+                time == 10 ? horaVariavelBRT = 10 : horaVariavelBRT;
+                time == 11 ? horaVariavelBRT = 9 : horaVariavelBRT;
+                time == 12 ? horaVariavelBRT = 8 : horaVariavelBRT;
+                time == 13 ? horaVariavelBRT = 7 : horaVariavelBRT;
+                time == 14 ? horaVariavelBRT = 6 : horaVariavelBRT;
+                time == 15 ? horaVariavelBRT = 5 : horaVariavelBRT;
+                time == 16 ? horaVariavelBRT = 4 : horaVariavelBRT;
+                time == 17 ? horaVariavelBRT = 3 : horaVariavelBRT;
+                time == 18 ? horaVariavelBRT = 2 : horaVariavelBRT;
+                time == 19 ? horaVariavelBRT = 1 : horaVariavelBRT;
+                time == 20 ? horaVariavelBRT = 24 : horaVariavelBRT;
+                time == 21 ? horaVariavelBRT = 23 : horaVariavelBRT;
+                time == 22 ? horaVariavelBRT = 22 : horaVariavelBRT;
+                time == 23 ? horaVariavelBRT = 21 : horaVariavelBRT;
+
+                let horarioEEST = 24 - horaVariavelEEST;
+                let horarioBRT = 24 - horaVariavelBRT;
+                const horariosConvertidos = ([{
+                    time: horarioEEST,
+                    timezone: "eest", 
+                },{
+                    time: horarioBRT,
+                    timezone: "brt",
+                }])
+                return horariosConvertidos;
+            }
+        }
     }
 }
 
